bf0b08e5722887ed0a38e3f7ec0754fc8daa05d2
Stepan <st.stepanov57@gmail.com>
2026-08-07 17:21:26 +0300

    add fix salary

--- app/services/salary_service.py
+++ app/services/salary_service.py
@@ -406,13 +406,20 @@ class SalaryService:
         advance = self._sum_operations_salary(employee_id, mounth_period, year, 5)
         bonus = self._sum_operations_salary(employee_id, mounth_period, year, 7)
         buh_total = self._sum_buh_salary(employee_id, mounth_period, year)
-        if buh_total == 0:
-            buh_total = self._sum_operations_salary(
-                employee_id, mounth_period, year, 1
-            ) + self._sum_operations_salary(employee_id, mounth_period, year, 2)
+        ops_salary = self._sum_operations_salary(
+            employee_id, mounth_period, year, 1
+        ) + self._sum_operations_salary(employee_id, mounth_period, year, 2)
+        buh_from_ops = False
+        if buh_total == 0 and ops_salary > 0:
+            buh_total = ops_salary
+            buh_from_ops = True
         vacation_buh = self._sum_buh_salary(employee_id, mounth_period, year, 4)
         vacation_ev = vacation_total - vacation_buh
-        paid_total = buh_total + self._sum_operations_salary(employee_id, mounth_period, year, exclude_type_ids=[1, 2, 3, 4, 7])
+        ops_other = self._sum_operations_salary(employee_id, mounth_period, year, exclude_type_ids=[1, 2, 3, 4, 7])
+        if buh_from_ops:
+            paid_total = buh_total + ops_other
+        else:
+            paid_total = buh_total + ops_salary + ops_other
         overpayment = (
             self._get_previous_overpayment(employee_id, mounth_period, year)
             if apply_overpayment
